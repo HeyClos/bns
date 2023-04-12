0e3920c6eb29e86859b21552e3974097ee3708fe
los <thiscarlos@gmail.com>
2023-04-12 14:11:32 -0700

    added copyright, aligned header text

--- app/page.js
+++ app/page.js
@@ -6,19 +6,19 @@ const inter = Inter({ subsets: ['latin'] })
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
+      <div className="z-10 w-full max-w-5xl items-center justify-end font-mono text-sm lg:flex">
         {/* <p className="fixed left-0 top-0 flex w-full justify-center border-b border-white-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30"> */}
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-white-300 pb-6 pt-8 backdrop-blur-2xl lg:static lg:w-auto  lg:rounded-xl lg:border lg:p-4">
+        {/* <p className="fixed left-0 top-0 flex w-full justify-center border-b border-white-300 pb-6 pt-8 backdrop-blur-2xl lg:static lg:w-auto  lg:rounded-xl lg:border lg:p-4">
           Share your recipes!
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
+        </p> */}
+        <div className="fixed left-0 flex h-48 w-full justify-center lg:static lg:h-auto lg:w-auto lg:bg-none">
             Made with
             <span class="w-5 h-5 ml-1 md:w-6 md:h-6 md:-mt-0.5 bg-cover mr-1.5 float-left rounded-sm bg-[url(https://img.icons8.com/fluency/512/hearts.png)]"></span>
             by
             <a class="mx-2 text-blue-600 hover:underline dark:text-gray-200" href="https://www.instagram.com/whatchaocooking/"> @whatchaocooking</a> 
         </div>
       </div>
-      <div className="relative flex place-items-center">
+      <div className="relative flex place-items-center justify-between">
       {/* <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]"> */}
         <a href='https://play.google.com/store/apps/details?id=com.kinpo.bns&pcampaignid=pcampaignidMKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1'>
           <Image
@@ -42,7 +42,7 @@ export default function Home() {
           />
         </a>
       </div>
-      <div className=" text-white mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
+      <div className=" text-white mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:flex lg:justify-center lg:text-left lg:content-end">
         <a
           href=""
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:shadow-indigo-500/40 hover:bg-blue-700/20 hover:dark:shadow-neutral-700/20 hover:dark:bg-neutral-800/30"
@@ -81,6 +81,17 @@ export default function Home() {
           </p>
         </a> 
       </div>
+      <footer className='flex justify-center text-sm text-white opacity-50 fixed bottom-4'>
+        © 2023 BNS. All rights reserved.
+        {/* <p>
+          Made with
+        </p>
+        <span class="w-5 h-5 ml-1 md:w-6 md:h-6 md:-mt-0.5 bg-cover mr-1.5 float-left rounded-sm bg-[url(https://img.icons8.com/fluency/512/hearts.png)]"></span>
+        by
+        <div>
+          <a class="mx-2 text-blue-600 hover:underline dark:text-gray-200" href="https://www.instagram.com/whatchaocooking/"> @whatchaocooking</a> 
+        </div> */}
+      </footer>
     </main>
   )
 }
