2ab988bbd4c97d9fa59be47c4d4758f84b524d7b
los <thiscarlos@gmail.com>
2023-04-17 15:57:10 -0700

    minimized padding to remove button downscaling

--- app/page.js
+++ app/page.js
@@ -5,13 +5,13 @@ const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-between p-4">
       <div className="z-10 w-full max-w-5xl items-center justify-end font-mono text-sm lg:flex">
         {/* <p className="fixed left-0 top-0 flex w-full justify-center border-b border-white-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30"> */}
         {/* <p className="fixed left-0 top-0 flex w-full justify-center border-b border-white-300 pb-6 pt-8 backdrop-blur-2xl lg:static lg:w-auto  lg:rounded-xl lg:border lg:p-4">
           Share your recipes!
         </p> */}
-        <div className=" left-0 flex justify-center static h-auto w-auto bg-none">
+        <div className="left-0 flex justify-center static h-auto w-auto bg-none">
             Made with <span class="w-5 h-5 ml-1 bg-cover mr-1.5 float-left rounded-sm bg-[url(https://img.icons8.com/fluency/512/hearts.png)]"></span>
             by
             <a class="mx-2 text-blue-600 hover:underline dark:text-gray-200" href="https://www.instagram.com/whatchaocooking/"> @whatchaocooking</a> 
@@ -19,7 +19,7 @@ export default function Home() {
       </div>
       <div className="relative flex place-items-center justify-between">
       {/* <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]"> */}
-        <a className="mx-5" href='https://play.google.com/store/apps/details?id=com.kinpo.bns&pcampaignid=pcampaignidMKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1'>
+        <a className="mx-5 inset-y-0 left-0" href='https://play.google.com/store/apps/details?id=com.kinpo.bns&pcampaignid=pcampaignidMKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1'>
           <Image
             className="relative "
             src="/google-play-badge.svg"
@@ -30,7 +30,7 @@ export default function Home() {
             priority
           />
         </a>
-        <a className="mx-5" href='https://apps.apple.com/us/app/beef-noodle-soup-bns/id1672179708'>
+        <a className="mx-5 inset-y-0 right-0" href='https://apps.apple.com/us/app/beef-noodle-soup-bns/id1672179708'>
           <Image
             className="relative "
             src="/app_store.svg"
