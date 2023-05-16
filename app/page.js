e4e1959911b4ac20cf9ec940dfd5efb5cd4e7a16
los <thiscarlos@gmail.com>
2023-05-15 18:03:21 -0700

    changed landingpg styling for how to use and our story

--- app/page.js
+++ app/page.js
@@ -11,14 +11,14 @@ const inter = Inter({ subsets: ['latin'] })
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-2">
-      <div className="z-10 w-full max-w-5xl items-center justify-center flex my-7">
-        <Image src={Logo} className="w-12 h-12 ml-1 bg-cover mr-1.5 float-left rounded-sm" />
-        <div className="text-white text-2xl font-semibold left-0 flex static h-auto w-auto bg-none">
+      <div className="z-10 w-full max-w-5xl items-center justify-center flex mt-4 mb-10">
+        <Image src={Logo} className="w-14 h-14 ml-1 bg-cover mr-1.5 float-left rounded-sm" />
+        <div className="text-white text-4xl font-semibold left-0 flex static h-auto w-auto bg-none">
             Beef Noodle Soup 
         </div>
       </div>
 
-      <div className="relative flex place-items-center justify-between mb-6">
+      <div className="relative flex place-items-center justify-between my-8">
       {/* <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]"> */}
         <a className="mx-5 inset-y-0 left-0 h-1/2" href='https://play.google.com/store/apps/details?id=com.kinpo.bns&pcampaignid=pcampaignidMKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1'>
           <Image
@@ -57,7 +57,7 @@ export default function Home() {
         </div>
       </div>
 
-      <div className="text-white z-10 w-full max-w-5xl items-center justify-center font-mono text-sm flex mb-7">
+      <div className="text-white z-10 w-full max-w-5xl items-center justify-center font-mono text-sm flex my-4">
         <div className="left-0 flex justify-center static h-auto w-auto bg-none">
             Made with <span className="w-5 h-5 ml-1 bg-cover mr-1.5 float-left rounded-sm bg-[url(https://img.icons8.com/fluency/512/hearts.png)]"></span>
             by
@@ -65,7 +65,7 @@ export default function Home() {
         </div>
       </div>
 
-      <div className=" text-white mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:flex lg:justify-center lg:text-left lg:content-end">
+      <div className=" text-white mb-10 grid-cols-4 flex justify-center text-left content-end my-3">
         <a
           href=""
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:shadow-indigo-500/40 hover:bg-blue-700/20 hover:dark:shadow-neutral-700/20 hover:dark:bg-neutral-800/30"
